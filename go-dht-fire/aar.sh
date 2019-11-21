fe42e34bbee187d3081de5c7beb9eabf976d84cb
李小明 <likeming@jasgroup.cn>
2019-11-21 17:50:18 +0800

    增加aar打包脚本.

---
+++ go-dht-fire/aar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+PROJECT="github.com/alx696/go-dht-fire-mp2p"
+PROJECT_PATH="$GOPATH/src/$PROJECT"
+
+echo "打包mp2p aar"
+
+# 复制源码
+mkdir -p $PROJECT_PATH
+cp -r mp2p $PROJECT_PATH
+
+# 复制依赖
+go mod vendor; cp -r vendor/* $GOPATH/src/; rm -rf $GOPATH/src/pkg $GOPATH/src/modules.txt ; rm -rf vendor
+
+# 打包(仅arm64)
+GO111MODULE="off"
+gomobile bind -v -o mp2p.aar -target=android/arm64 $PROJECT
